f18050b3b1295406f24e919a83e445b33883eb82
Redgale <150551630+Redgale@users.noreply.github.com>
2025-09-18 18:36:32 -0400

    Create games.js

---
+++ games.js
@@ -0,0 +1,13 @@
+window.GAME_LIST = [
+  { name: "Cookie Clicker", url: "https://cookieclicker-nu.vercel.app" },
+  { name: "Drive Mad", url: "https://drive-mad1.vercel.app" },
+  { name: "Eaglercraft 1.8", url: "https://eagler1-8-8-wasm-gc.vercel.app" },
+  { name: "Eaglercraft 1.12", url: "https://math-class-school.vercel.app" },
+  { name: "House of Hazards", url: "https://house-of-hazards-ashy.vercel.app" },
+  { name: "Dadish 1", url: "https://dadish.vercel.app" },
+  { name: "Bad Monday Sim", url: "https://badmondaysim.vercel.app/" },
+  { name: "Dadish 2", url: "https://dadish2.vercel.app/" },
+  { name: "Stickman Hook", url: "https://stickman-hook-five.vercel.app" },
+  { name: "Bitlife", url: "https://bitlife1.vercel.app" },
+  { name: "Compact Conflict", url: "https://wasyl.eu/games/compact-conflict/play.html" },
+];
